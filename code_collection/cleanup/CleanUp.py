382cd477e517333c6922f38a2aee73279eee0d05
puffycodes <112615643+puffycodes@users.noreply.github.com>
2024-11-22 01:03:30 +0800

    Add files via upload

---
+++ code_collection/cleanup/CleanUp.py
@@ -0,0 +1,32 @@
+import os
+
+homeDir="C:/practice" #where the files are in the directory,
+totalSize= 0
+numberTextFile = 0
+print("List of text files:")
+for txtFile in os.listdir(homeDir): #accessing the files from the folder.
+
+    fileName, fileExtension = os.path.splitext(txtFile)
+    if fileExtension == ".txt": #everything text file related
+        logFileLocation = os.path.join(homeDir, txtFile) #getting the full path of the text file
+        fileSize = os.path.getsize(logFileLocation) #getting the file size
+        totalSize = totalSize + fileSize #adding up every file size
+        numberTextFile = numberTextFile + 1
+        print(logFileLocation, "=" , fileSize ,"bytes")
+
+averageSize = int(totalSize / numberTextFile) #finding the average of the file
+print("Total file size: ",  totalSize ,"bytes")
+print()
+print("Total number of text files:" ,numberTextFile)
+print()
+print("Average size of all text files:" ,averageSize ,"bytes")
+print()
+
+print("List of non-text files")
+for txtFile in os.listdir(homeDir): #accessing the files from the folder.
+
+    fileName, fileExtension = os.path.splitext(txtFile)
+    if fileExtension != ".txt": #not txt file
+        logFileLocation = os.path.join(homeDir, txtFile)  # getting the full path of the text file
+
+        print(logFileLocation,)
