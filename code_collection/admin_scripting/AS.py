567179e5d14ee337063e1a7430c3e6a0febb4364
puffycodes <as1234dfgh@gmail.com>
2025-02-06 20:37:29 +0800

    add code for admin scripting

---
+++ code_collection/admin_scripting/AS.py
@@ -0,0 +1,50 @@
+
+import os
+import socket
+import shutil
+import logging
+from datetime import datetime
+
+# define the locations of the files
+top_folder = os.getcwd()
+resource_folder = os.path.join(top_folder, 'resource')
+
+log_file = os.path.join(resource_folder, 'log.txt')
+
+print(f'top folder: {top_folder}')
+print(f'log file: {log_file}')
+
+# define the logging file location
+logging.basicConfig(filename='admin_script_log.txt')
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+# get the current date and time and hostname
+time_now = datetime.now()
+current_date = time_now.strftime('%Y%m%d')
+current_time = time_now.strftime('%H%M%S')
+hostname = socket.gethostname()
+
+print(f'date: {current_date}, time: {current_time}')
+print(f'hostname: {hostname}')
+
+# process the log file
+
+with open(log_file, 'r') as log_fd:
+    for line in log_fd:
+        #print(line.strip())
+        filename, owner = line.strip().split(':')
+        #print(filename, owner)
+
+        current_file = os.path.join(resource_folder, filename)
+        if os.path.isfile(current_file):
+            print(current_file)
+
+            name, ext = os.path.splitext(filename)
+            new_name = f'{name}_{owner}{ext}'
+            print(name, ext, '->', new_name)
+
+            move_to_filename = os.path.join(resource_folder, new_name)
+
+            os.rename(current_file, move_to_filename)
+            logger.info(f'renamed: {current_file} -> {move_to_filename}')
